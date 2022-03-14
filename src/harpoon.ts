bdeaf157afab32502bda1801387fbc296736ac64
tobiaszimmermann <cph-tz11@cphbusiness.dk>
2022-03-14 22:27:19 +0100

    create dev branch

--- src/harpoon.ts
+++ src/harpoon.ts
@@ -19,5 +19,4 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
