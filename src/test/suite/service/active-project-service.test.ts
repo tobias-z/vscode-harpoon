c5c325d4c4a6688f190d1a2b95115015666e1ad5
tobiaszimmermann <cph-tz11@cphbusiness.dk>
2022-03-20 21:20:51 +0100

    test: active editor service

---
+++ src/test/suite/service/active-project-service.test.ts
@@ -0,0 +1,19 @@
+import ActiveProjectService from "../../../service/active-project-service";
+import * as assert from "assert";
+import { beforeEach } from "mocha";
+
+suite("active project service", () => {
+  let activeProjectService: ActiveProjectService;
+
+  beforeEach(() => {
+    activeProjectService = new ActiveProjectService();
+  });
+
+  test("can add editor", () => {
+    const expected = "something";
+    activeProjectService.addEditor({ fileName: expected, lastLine: 1 });
+    const actual = activeProjectService.getEditor(1);
+    assert.notStrictEqual(actual, null);
+    assert.strictEqual(actual.fileName, expected);
+  });
+});
