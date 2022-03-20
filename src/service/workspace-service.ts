742024c0687364136ce894baf81ec383d4fa85cd
tobiaszimmermann <cph-tz11@cphbusiness.dk>
2022-03-20 22:04:56 +0100

    feat: can change workspace + test changing workspace based on id

--- src/service/workspace-service.ts
+++ src/service/workspace-service.ts
@@ -1,14 +1,16 @@
 import * as vscode from "vscode";
-import { getInstance } from "../util/singleton";
 import ActiveProjectService from "./active-project-service";
 
 export default class WorkspaceService {
   constructor(private readonly activeProjectService: ActiveProjectService) {}
 
   public changeWorkspace(id: number) {
-    const { fileName } = this.activeProjectService.getEditor(id);
-    vscode.workspace.openTextDocument(fileName).then(doc => {
-      vscode.window.showTextDocument(doc);
+    const editor = this.activeProjectService.getEditor(id);
+    if (!editor) {
+      return;
+    }
+    return vscode.workspace.openTextDocument(editor.fileName).then(doc => {
+      return vscode.window.showTextDocument(doc);
     });
   }
 }
