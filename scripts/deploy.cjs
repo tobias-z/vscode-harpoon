4ca3e541e19491793d0b57a359d72ed975adb940
tobiaszimmermann <cph-tz11@cphbusiness.dk>
2022-04-02 16:37:07 +0200

    feat: complete ci workflow

--- scripts/deploy.cjs
+++
@@ -1,9 +0,0 @@
-const executeCommand = require("./command-runner.cjs");
-const { process } = require("./utils.cjs");
-
-async function deploy() {
-  executeCommand("yarn update-version");
-  executeCommand("vsce publish --yarn");
-}
-
-process(deploy);
