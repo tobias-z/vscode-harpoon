dba7221e3e7ee5b1ea578099f205188247992216
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2023-02-14 18:15:08 +0100

    feat: update version

--- scripts/version.cjs
+++ scripts/version.cjs
@@ -4,35 +4,35 @@ const executeCommand = require("./command-runner.cjs");
 const { getArgs, handleProcess } = require("./utils.cjs");
 
 async function version() {
-  const args = getArgs();
-  const isMinor = args.includes("--minor");
-  if (!args.includes("--major") && !isMinor) {
-    executeCommand("npm version patch");
-    return;
-  }
+    const args = getArgs();
+    const isMinor = args.includes("--minor");
+    if (!args.includes("--major") && !isMinor) {
+        executeCommand("npm version patch");
+        return;
+    }
 
-  const packageJson = await getPackageJson();
-  packageJson.version = getNewVersion(packageJson.version, isMinor);
-  await fs.writeFile(getPath(), JSON.stringify(packageJson));
-  executeCommand(`git tag v${packageJson.version}`);
+    const packageJson = await getPackageJson();
+    packageJson.version = getNewVersion(packageJson.version, isMinor);
+    await fs.writeFile(getPath(), JSON.stringify(packageJson));
+    executeCommand(`git tag v${packageJson.version}`);
 }
 
 function getNewVersion(currentVersion, isMinor) {
-  if (isMinor) {
-    const oldMinor = currentVersion.split(".")[1];
-    return `${parseFloat(currentVersion)}.${parseInt(oldMinor) + 1}.0`;
-  }
-  const newMajor = parseFloat(currentVersion) + 1;
-  return `${newMajor}.0.0`;
+    if (isMinor) {
+        const oldMinor = currentVersion.split(".")[1];
+        return `${parseFloat(currentVersion)}.${parseInt(oldMinor) + 1}.0`;
+    }
+    const newMajor = parseFloat(currentVersion) + 1;
+    return `${newMajor}.0.0`;
 }
 
 async function getPackageJson() {
-  const packageJson = await fs.readFile(getPath());
-  return JSON.parse(packageJson);
+    const packageJson = await fs.readFile(getPath());
+    return JSON.parse(packageJson);
 }
 
 function getPath() {
-  return path.resolve(__dirname, "../package.json");
+    return path.resolve(__dirname, "../package.json");
 }
 
 handleProcess(version);
